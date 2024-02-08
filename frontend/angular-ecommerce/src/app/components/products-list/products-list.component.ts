f9e7c39f23882150b72e0adea6857e9314c49ad5
Gerald Oliva <geraldsamoliva@gmail.com>
2024-02-07 22:00:26 -0500

    Build HTML table

--- frontend/angular-ecommerce/src/app/components/products-list/products-list.component.ts
+++ frontend/angular-ecommerce/src/app/components/products-list/products-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../common/product';
 
 @Component({
   selector: 'app-products-list',
-  templateUrl: './products-list.component.html',
-  styleUrl: './products-list.component.css'
+  templateUrl: './products-list-table.component.html',
+  styleUrl: './products-list.component.css',
 })
-export class ProductsListComponent {
+export class ProductsListComponent implements OnInit {
+  products: Product[] = [];
 
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    this.listProducts();
+  }
+
+  listProducts() {
+    this.productService.getProductList().subscribe((data) => {
+      this.products = data;
+    });
+  }
 }
