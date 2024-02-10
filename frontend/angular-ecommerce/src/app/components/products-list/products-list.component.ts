36e3b8c49d9d172b63642ac64eab53177c2c4c79
Gerald Oliva <geraldsamoliva@gmail.com>
2024-02-09 22:05:05 -0500

    Enhance ProductList

--- frontend/angular-ecommerce/src/app/components/products-list/products-list.component.ts
+++ frontend/angular-ecommerce/src/app/components/products-list/products-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../common/product';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-products-list',
@@ -9,15 +10,33 @@ import { Product } from '../../common/product';
 })
 export class ProductsListComponent implements OnInit {
   products: Product[] = [];
+  currentCategoryId: number = 1;
 
-  constructor(private productService: ProductService) {}
+  constructor(
+    private productService: ProductService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.listProducts();
+    this.route.paramMap.subscribe(() => {
+      this.listProducts();
+    });
   }
 
   listProducts() {
-    this.productService.getProductList().subscribe((data) => {
+    // check if "id" parameter is available
+    const hasCategoryId: boolean = this.route.snapshot.paramMap.has('id');
+
+    if (hasCategoryId) {
+      // get the "id' param string. convert string to a number using the "+" symbol
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    } else {
+      // no category id available, default to category id 1
+      this.currentCategoryId = 1;
+    }
+
+    // now get the products for the given id
+    this.productService.getProductList(this.currentCategoryId).subscribe((data) => {
       this.products = data;
     });
   }
--- frontend/angular-ecommerce/src/app/services/product.service.ts
+++ frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -10,7 +10,9 @@ export class ProductService {
   private baseUrl = 'http://localhost:8080/api/products';
   constructor(private httpClient: HttpClient) {}
 
-  getProductList(): Observable<Product[]> {
+  getProductList(theCategoryId: number): Observable<Product[]> {
+    // @TODO: need to build URL based on category id...
+
     return this.httpClient
       .get<GetResponse>(this.baseUrl)
       .pipe(map((response) => response._embedded.products));
